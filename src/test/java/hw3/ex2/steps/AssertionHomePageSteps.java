eb914a79e43b155afd22b8b2a6b694d88ba7e44d
Aleksandr <asponomar@gmail.com>
2019-12-10 20:55:37 +0300

    allure added

--- src/test/java/hw3/ex2/steps/AssertionHomePageSteps.java
+++ src/test/java/hw3/ex2/steps/AssertionHomePageSteps.java
@@ -2,6 +2,7 @@ package hw3.ex2.steps;
 
 import hw3.ex2.*;
 import hw3.ex2.components.*;
+import io.qameta.allure.*;
 import org.openqa.selenium.*;
 
 import java.util.*;
@@ -22,14 +23,17 @@ public class AssertionHomePageSteps {
         this.footer = new Footer(driver);
     }
 
+    @Step("Browser title '{0}' on the Home Page")
     public void browserTitleShouldBe(String browserTitle) {
         assertEquals(homePage.getPageTitle(), browserTitle);
     }
 
+    @Step("Username should be '{0}'")
     public void userNameShouldBe(String userName) {
         assertEquals(headerMenu.getUserNameText(), userName);
     }
 
+    @Step("Header menu buttons should be '{0}'")
     public void headerSectionMenuButtonsShouldBe(List<String> headerSectionMenuButtons) {
         IntStream.range(0, headerSectionMenuButtons.size()).forEach(i -> {
             assertTrue(headerMenu.getHeaderSectionMenuButtons().get(i).isDisplayed());
@@ -37,6 +41,7 @@ public class AssertionHomePageSteps {
         });
     }
 
+    @Step("Images should be displayed on the Home Page")
     public void imagesOnHomePageShouldBeDisplayed() {
         assertEquals(homePage.getHomePageImages().size(), 4);
         for (WebElement element : homePage.getHomePageImages()) {
@@ -44,51 +49,63 @@ public class AssertionHomePageSteps {
         }
     }
 
+    @Step("Text under icons should be '{0}' on the Home Page")
     public void homePageUnderIconsTextShouldBe(List<String> underIconsText) {
         IntStream.range(0, underIconsText.size())
                 .forEach(i -> assertEquals(homePage.getBenefitText().get(i).getText(), underIconsText.get(i)));
     }
 
+    @Step("Central main header should be displayed on the Home Page")
     public void centralMainHeaderIsDisplayed() {
         assertTrue(homePage.getCentralMainHeader().isDisplayed());
     }
 
+    @Step("Text under main header should be displayed on the Home Page")
     public void centralMainHeaderTextIsDisplayed() {
         assertTrue(homePage.getCentralMainHeaderText().isDisplayed());
     }
 
+    @Step("Central main header should be '{0}' on the Home Page")
     public void centralMainHeaderShouldBe(String centralMainHeader) {
         assertEquals(homePage.getCentralMainHeader().getText(), centralMainHeader);
     }
 
+    @Step("Text under main header should be '{0}' on the Home Page")
     public void centralMainHeaderTextShouldBe(String centralMainHeaderText) {
         assertEquals(homePage.getCentralMainHeaderText().getText(), centralMainHeaderText);
     }
 
+    @Step("Frame should be displayed on the Home Page")
     public void iFrameIsDisplayed() {
         assertTrue(homePage.getIFrame().isDisplayed());
     }
 
+    @Step("EPAM logo shoulde be displayed in the frame on the Home Page")
     public void epamLogoInIFrameIsDisplayed() {
         assertTrue(homePage.getEpamLogo().isDisplayed());
     }
 
+    @Step("Subheader text should be displayed on the Home Page")
     public void subHeaderTextIsDisplayed() {
         assertTrue(homePage.getSubHeaderText().isDisplayed());
     }
 
+    @Step("Subheader text should be '{0}' on the Home Page")
     public void subHeaderTextShouldBe(String subHeaderText) {
         assertEquals(homePage.getSubHeaderText().getText(), subHeaderText);
     }
 
+    @Step("JDI Github link should be '{0}' on the Home Page")
     public void jDIGitHubLinkShouldBe(String link) {
         assertEquals(homePage.getEpamJdiGitHubLink().getAttribute("href"), link);
     }
 
+    @Step("Left section should be displayed")
     public void leftSectionIsDisplayed() {
         assertTrue(navigationSideBar.getLeftSection().isDisplayed());
     }
 
+    @Step("Footer should be displayed")
     public void footerIsDisplayed() {
         assertTrue(footer.getFooter().isDisplayed());
     }
--- src/test/java/hw5/HomePageTestFailed.java
+++ src/test/java/hw5/HomePageTestFailed.java
@@ -16,7 +16,6 @@ public class HomePageTestFailed extends AbstractBaseTest implements TestData {
         assertSteps = new AssertionHomePageSteps(driver);
     }
 
-    //        2 Assert Browser title
     @Test
     public void browserTitleTest() {
         actionSteps.open("HOME_PAGE_URL");
@@ -37,93 +36,4 @@ public class HomePageTestFailed extends AbstractBaseTest implements TestData {
         assertSteps.browserTitleShouldBe(BROWSER_TITLE);
     }
 
-    @Test
-    public void headerSectionMenuButtonsTest() {
-        actionSteps.open(HOME_PAGE_URL);
-        assertSteps.headerSectionMenuButtonsShouldBe(HEADER_SECTION_MENU_BUTTONS);
-    }
-
-    @Test
-    public void homePageTestImages() {
-        actionSteps.open(HOME_PAGE_URL);
-        assertSteps.imagesOnHomePageShouldBeDisplayed();
-    }
-
-    @Test
-    public void homePageUnderIconsTextTest() {
-        actionSteps.open(HOME_PAGE_URL);
-        assertSteps.homePageUnderIconsTextShouldBe(UNDER_ICONS_TEXT);
-    }
-
-    @Test
-    public void centralMainHeaderIsDisplayed() {
-        actionSteps.open(HOME_PAGE_URL);
-        assertSteps.centralMainHeaderIsDisplayed();
-    }
-
-    @Test
-    public void centralMainHeaderTest() {
-        actionSteps.open(HOME_PAGE_URL);
-        assertSteps.centralMainHeaderShouldBe("CENTRAL_MAIN_HEADER");
-    }
-
-    @Test
-    public void centralMainHeaderTextIsDisplayed() {
-        actionSteps.open(HOME_PAGE_URL);
-        assertSteps.centralMainHeaderTextIsDisplayed();
-    }
-
-    @Test
-    public void centralMainHeaderTextTest() {
-        actionSteps.open(HOME_PAGE_URL);
-        assertSteps.centralMainHeaderTextShouldBe(CENTRAL_MAIN_HEADER_TEXT);
-    }
-
-    @Test
-    public void iframeIsDisplayedTest() {
-        actionSteps.open(HOME_PAGE_URL);
-        actionSteps.login(USER_LOGIN, USER_PASSWORD);
-        actionSteps.swithToIFrame();
-        assertSteps.iFrameIsDisplayed();
-    }
-
-    @Test
-    public void epamLogoInIFrameIsDisplayedTest() {
-        actionSteps.open(HOME_PAGE_URL);
-        actionSteps.login(USER_LOGIN, USER_PASSWORD);
-        actionSteps.swithToIFrame();
-        assertSteps.epamLogoInIFrameIsDisplayed();
-    }
-
-    @Test
-    public void subHeaderTextIsDisplayedTest() {
-        actionSteps.open(HOME_PAGE_URL);
-        assertSteps.subHeaderTextIsDisplayed();
-    }
-
-    @Test
-    public void subHeaderTextShouldBe() {
-        actionSteps.open(HOME_PAGE_URL);
-        assertSteps.subHeaderTextShouldBe("SUB_HEADER_TEXT");
-    }
-
-    @Test
-    public void jDIGitHubIsALinkTest() {
-        actionSteps.open(HOME_PAGE_URL);
-        assertSteps.jDIGitHubLinkShouldBe(JDI_GITHUB_LINK);
-    }
-
-    @Test
-    public void leftSectionIsDisplayedTest() {
-        actionSteps.open(HOME_PAGE_URL);
-        assertSteps.leftSectionIsDisplayed();
-    }
-
-    @Test
-    public void footerIsDisplayedTest() {
-        actionSteps.open("HOME_PAGE_URL");
-        assertSteps.footerIsDisplayed();
-    }
-
 }
-
